ad38fb76469d9e0d3f2209782601b322ee51e22d
Amanda Rodgers <aarodgerswork@gmail.com>
2019-07-19 18:50:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,7 +53,6 @@ end
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
     no_offense_steven = instructors.delete("Steven")
-    instructors
 end
 
 
